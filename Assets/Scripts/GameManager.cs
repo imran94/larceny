1e9a3d3717abb07c509d0f05bdd3d20a21a8363e
imran94 <iraja094@gmail.com>
2016-08-16 00:52:46 +0800

    Create GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public TileMap tileScript;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		tileScript = GetComponent<TileMap> ();
+		InitGame ();
+	}
+
+	void InitGame ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
