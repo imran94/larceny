cd2a7dd3a78154ad860a571e399efec312891aba
blackmetall95 <rzcc5911@yahoo.co.uk>
2016-08-17 06:18:19 +0800

    Loading Screen and BGM
    -Added a Loading Screen (Image)
    -Added placeholder BGM for the Menu scene
    -Added code for playing level 1 music (not currently used)

---
+++ Assets/Scripts/PreserveOnLoad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreserveOnLoad : MonoBehaviour {
+    //Initialization
+    //public AudioClip placeholdername;
+    //private AudioSource source;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        //For the destruction of BGM_Menu when there is more than one instance
+        //if (GameObject.FindGameObjectsWithTag("Music_Menu").Length > 1)
+        //{
+        //    Destroy(gameObject);
+        //}
+
+        //source = GetComponent<AudioSource>(); //Gets audio file
+    }
+
+    void OnLevelWasLoaded (int sceneIndex)
+    {
+        if (sceneIndex == 1)
+        {
+            Destroy(gameObject);
+            //Implementation for BGM_Lvl1
+            //source.clip = placeholdername;
+            //source.Play();
+        }
+    }
+}
