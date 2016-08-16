2abb79796a67f0104f14d8cc914f1884fccb0668
blackmetall95 <rzcc5911@yahoo.co.uk>
2016-08-17 06:23:21 +0800

    Failure to commit test 2

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -3,8 +3,12 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
+    public GameObject LoadingImg;
+
     public void LoadScene (int sceneIndex)
     {
+        LoadingImg.SetActive(true);
         SceneManager.LoadScene(sceneIndex);
     }
+
 }
