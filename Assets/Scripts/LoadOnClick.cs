5d937349e9dc348c8d15a3e1cc277982db4ae4a6
blackmetall95 <rzcc5911@yahoo.co.uk>
2016-08-18 00:23:31 +0800

    Level Complete Overlay
    -Added an overlay for winning. Triggered by a button for now
    -Added return to menu button. (Works)

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,11 +4,31 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
     public GameObject LoadingImg;
+    public GameObject WinImg;
 
     public void LoadScene (int sceneIndex)
     {
-        LoadingImg.SetActive(true);
-        SceneManager.LoadScene(sceneIndex);
+        LoadingImg.SetActive(true); //Displays loading screen overlay
+        SceneManager.LoadScene(sceneIndex); //Loads scene based on the index of each scene
     }
 
+    //public void LoadMenu( int sceneIndex)
+    //{
+    //    SceneManager.LoadScene(sceneIndex); 
+    //}
+
+    public void OnClickWin ()  //Temporary way to enable WinImg
+    {
+        WinImg.SetActive(true);
+    }
+
+    //public void OnClickRestart()
+    //{
+
+    //}
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
 }
