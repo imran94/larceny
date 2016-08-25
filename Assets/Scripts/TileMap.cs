4603cc69d6646a1b5e61037fa8aa636e05abb39e
imran94 <iraja094@gmail.com>
2016-08-25 09:50:38 +0800

    Make changes to character prefabs

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -21,48 +21,18 @@ public class TileMap : MonoBehaviour {
 
     public void Start()
     {
-        generateMapData();
-        generateMapVisual();
     }
     
-    void generateMapData()
+    void generateTileData()
     {
         GameObject walkable = (GameObject)Instantiate (Resources.Load ("Walkable"));
         GameObject nonWalkable = (GameObject)Instantiate (Resources.Load ("NonWalkable"));
 		GameObject exit = (GameObject)Instantiate (Resources.Load ("Exit"));
 
-        player = GameObject.Find("Player");
-
         tileTypes = new TileType[3];
         tileTypes [WALKABLE] = new TileType (walkable, true);
         tileTypes [NONWALKABLE] = new TileType (nonWalkable, false);
 		tileTypes [EXIT] = new TileType (exit, true);
-
-		tiles = new TileType[mapSizeX, mapSizeZ];
-
-        for (int x = 0; x < mapSizeX; x++)
-        {
-            for (int z = 0; z < mapSizeZ; z++)
-            {
-				tilePositions.Add(new Vector3(x, 0f, z));
-                tiles[x, z] = tileTypes[NONWALKABLE];
-            }
-        }
-
-		// Convert to a function
-		tiles [4, 4] = tileTypes [WALKABLE];
-		tiles [4, 5] = tileTypes [WALKABLE];
-        tiles [4, 6] = tileTypes [WALKABLE];
-		tiles [4, 7] = tileTypes [WALKABLE];
-		tiles [4, 8] = tileTypes [WALKABLE];
-
-		tiles [5, 8] = tileTypes [WALKABLE];
-		tiles [6, 8] = tileTypes [WALKABLE];
-		tiles [7, 8] = tileTypes [WALKABLE];
-
-		tiles [8, 8] = tileTypes [EXIT];
-
-        player.transform.position = new Vector3(4f, 3f, 4f);
     }
 
     void generateMapVisual()
@@ -83,7 +53,93 @@ public class TileMap : MonoBehaviour {
         }
     }
 
-	// Update is called once per frame
-	void Update () {
+    public void generateLevel(int level)
+    {
+        generateTileData();
+
+        switch (level)
+        {
+            case 1:
+                generateLevel1();
+                break;
+        }
+
+        generateMapVisual();
+    }
+
+    void generateLevel1()
+    {
+
+        tiles = new TileType[mapSizeX, mapSizeZ];
+        Debug.Log(tileTypes.Length);
+
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int z = 0; z < mapSizeZ; z++)
+            {
+                tilePositions.Add(new Vector3(x, 0f, z));
+                tiles[x, z] = tileTypes[NONWALKABLE];
+            }
+        }
+
+        //tiles[4, 4] = tileTypes[WALKABLE];
+        //tiles[4, 5] = tileTypes[WALKABLE];
+        //tiles[4, 6] = tileTypes[WALKABLE];
+        //tiles[4, 7] = tileTypes[WALKABLE];
+        //tiles[4, 8] = tileTypes[WALKABLE];
+
+        //tiles[5, 8] = tileTypes[WALKABLE];
+        //tiles[6, 8] = tileTypes[WALKABLE];
+        //tiles[7, 8] = tileTypes[WALKABLE];
+
+        //tiles[8, 8] = tileTypes[EXIT];
+
+        tiles[0, 0] = tileTypes[WALKABLE];
+        tiles[1, 0] = tileTypes[WALKABLE];
+        tiles[2, 0] = tileTypes[WALKABLE];
+        tiles[3, 0] = tileTypes[WALKABLE];
+        tiles[4, 0] = tileTypes[WALKABLE];
+        tiles[5, 0] = tileTypes[WALKABLE];
+        tiles[6, 0] = tileTypes[WALKABLE];
+
+        tiles[0, 1] = tileTypes[WALKABLE];
+        tiles[0, 2] = tileTypes[WALKABLE];
+        tiles[0, 3] = tileTypes[WALKABLE];
+        tiles[0, 4] = tileTypes[WALKABLE];
+        tiles[0, 5] = tileTypes[WALKABLE];
+        tiles[0, 6] = tileTypes[WALKABLE];
+
+        tiles[2, 1] = tileTypes[WALKABLE];
+        tiles[2, 2] = tileTypes[WALKABLE];
+        tiles[2, 3] = tileTypes[WALKABLE];
+        tiles[2, 4] = tileTypes[WALKABLE];
+        tiles[2, 5] = tileTypes[WALKABLE];
+        tiles[2, 6] = tileTypes[WALKABLE];
+
+        tiles[4, 2] = tileTypes[WALKABLE];
+        tiles[4, 3] = tileTypes[WALKABLE];
+        tiles[4, 4] = tileTypes[WALKABLE];
+        tiles[4, 6] = tileTypes[WALKABLE];
+
+        tiles[6, 1] = tileTypes[WALKABLE];
+        tiles[6, 2] = tileTypes[WALKABLE];
+        tiles[6, 3] = tileTypes[WALKABLE];
+        tiles[6, 4] = tileTypes[WALKABLE];
+        tiles[6, 5] = tileTypes[WALKABLE];
+        tiles[6, 6] = tileTypes[WALKABLE];
+
+        tiles[1, 6] = tileTypes[WALKABLE];
+        tiles[3, 6] = tileTypes[WALKABLE];
+        tiles[5, 6] = tileTypes[WALKABLE];
+        tiles[6, 6] = tileTypes[WALKABLE];
+
+        tiles[6, 7] = tileTypes[EXIT];
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	}
 }
