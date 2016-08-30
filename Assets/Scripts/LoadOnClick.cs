9813ca0edba965b1f90190354a3a244406ae091b
blackmetall95 <rzcc5911@yahoo.co.uk>
2016-08-30 13:56:50 +0800

    Integrated menu

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,18 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
     public GameObject LoadingImg;
-    public GameObject WinImg;
 
     public void LoadScene (int sceneIndex)
     {
         LoadingImg.SetActive(true); //Displays loading screen overlay
         SceneManager.LoadScene(sceneIndex); //Loads scene based on the index of each scene
     }
-
-    //public void OnClickWin ()  //Temporary way to enable WinImg
-    //{
-    //    WinImg.SetActive(true);
-    //}
 
     public void OnClickExit()
     {
