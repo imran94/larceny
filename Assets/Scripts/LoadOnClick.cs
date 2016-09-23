4c2611d356d52448eca3af5d803bf146a8b6c1d6
blackmetall95 <rzcc5911@yahoo.co.uk>
2016-09-23 16:39:48 +0800

    Re-Added features
    Pause Menu
    Next Button
    Level Indicator

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -43,7 +43,6 @@ public class LoadOnClick : MonoBehaviour {
     public void LevelTransition (int sceneIndex)
     {
         Loader.level++;
-        //LevelText.levelNum++;
         SceneManager.LoadScene(sceneIndex);
     }
 
